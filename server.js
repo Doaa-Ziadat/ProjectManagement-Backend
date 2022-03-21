e5196935df578125be6e5ecb16fb08df58524f1a
Doaa ziadat <doaaziadat@gmail.com>
2022-03-21 12:42:53 +0200

    .

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(cors());
-app.all("*", function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type,Content-Length, Authorization, Accept,X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
+
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
