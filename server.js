3acf30370a50daea6bbac96825d6bbdfaf4d3e80
Doaa ziadat <doaaziadat@gmail.com>
2022-03-21 12:24:24 +0200

    .

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
