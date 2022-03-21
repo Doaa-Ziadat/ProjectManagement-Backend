0a2b59494bfb25f975ecac315b082c7c92675983
Doaa ziadat <doaaziadat@gmail.com>
2022-03-21 12:47:44 +0200

    .

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const router = require("./router");
-const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
-
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
